4a91a5aff464449f91e36355b65499c78d21b55e
Diego Figueroa <diego.figueroa@aurora.dev>
2023-06-08 14:39:01 -0400

    feat: add CustomEntityPage to dev server

--- plugins/blockchain-frontend/dev/index.tsx
+++ plugins/blockchain-frontend/dev/index.tsx
@@ -1,28 +1,42 @@
 import React from 'react';
 import { createDevApp } from '@backstage/dev-utils';
 import { Content, Header, Page } from '@backstage/core-components';
-// import { createApiFactory, createPlugin } from '@backstage/core-plugin-api';
-import { CatalogIndexPage, catalogPlugin } from '@backstage/plugin-catalog';
-// import {
-//   CatalogApi,
-//   MockStarredEntitiesApi,
-//   catalogApiRef,
-//   starredEntitiesApiRef,
-//   entityRouteRef,
-// } from '@backstage/plugin-catalog-react';
+import {
+  CatalogEntityPage,
+  CatalogIndexPage,
+  catalogPlugin,
+} from '@backstage/plugin-catalog';
+import { CatalogGraphPage, catalogGraphPlugin } from '@backstage/plugin-catalog-graph';
+import { apiDocsPlugin } from '@backstage/plugin-api-docs';
+import { orgPlugin } from '@backstage/plugin-org';
+import { pagerDutyPlugin } from '@backstage/plugin-pagerduty';
 import { Grid, Typography } from '@material-ui/core';
 
 import { customCatalogPagePlugin } from '../src/plugin';
 import { CustomCatalogPage } from '../src/components/CustomCatalogPage';
+import { CustomEntityPage } from '../src/components/CustomEntityPage';
 
 createDevApp()
   .registerPlugin(catalogPlugin)
+  .registerPlugin(orgPlugin)
+  .registerPlugin(catalogGraphPlugin)
+  .registerPlugin(apiDocsPlugin)
+  .registerPlugin(pagerDutyPlugin)
   .registerPlugin(customCatalogPagePlugin)
   .addPage({
     element: <CatalogIndexPage />,
     title: 'Root Page',
     path: '/catalog',
-    children: <CustomCatalogPage />
+    children: <CustomCatalogPage />,
+  })
+  .addPage({
+    path: '/catalog/:kind/:namespace/:name',
+    element: <CatalogEntityPage />,
+    children: <CustomEntityPage />,
+  })
+  .addPage({
+    path: '/catalog-graph',
+    element: <CatalogGraphPage />,
   })
   .addPage({
     element: (
@@ -34,9 +48,7 @@ createDevApp()
             <Grid item xs={12} md={12}>
               <Typography variant="h4">Test homepage</Typography>
             </Grid>
-
           </Grid>
-
         </Content>
       </Page>
     ),
---
+++ plugins/blockchain-frontend/src/components/CustomEntityPage/index.ts
@@ -0,0 +1,1 @@
+export { CustomEntityPage } from './CustomEntityPage';
