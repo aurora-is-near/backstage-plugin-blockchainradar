5765c2e162f03c6cfacae4baaf9ad1fcbc0ac307
Diego Figueroa <diego.figueroa@aurora.dev>
2023-07-28 19:34:19 -0400

    feat: add initial openzeppelin roles and membership parsing

--- plugins/blockchainradar-backend/src/processors/ContractProcessor.ts
+++ plugins/blockchainradar-backend/src/processors/ContractProcessor.ts
@@ -1,5 +1,4 @@
 import {
-  CatalogProcessorCache,
   CatalogProcessorEmit,
   processingResult,
 } from '@backstage/plugin-catalog-node';
@@ -13,9 +12,23 @@ import {
 
 import { ContractComponent } from '../entities/ContractComponent';
 import { BlockchainFactory } from '../lib/BlockchainFactory';
+import { OpenZeppelinClient } from '../lib/OpenZeppelinClient';
 import { BlockchainProcessor } from './BlockchainProcessor';
 
 export class ContractProcessor extends BlockchainProcessor {
+  async postProcessEntity?(
+    entity: Entity,
+    location: LocationSpec,
+    emit: CatalogProcessorEmit,
+  ) {
+    if (isContractComponent(entity)) {
+      await this.processContractComponent(entity, location, emit);
+    } else if (isContractDeployment(entity)) {
+      await this.processContractDeployment(entity, location, emit);
+    }
+    return entity;
+  }
+
   /**
    * Emits deployments (API kind) from deployedAt/interactsWith specs
    * also appends etherscan links
@@ -83,6 +96,7 @@ export class ContractProcessor extends BlockchainProcessor {
           },
         );
       }
+
       if (!this.isCacheUpToDate(deploymentSpec.state)) {
         await this.runExclusive(
           'deployment-state-fetch',
@@ -100,7 +114,33 @@ export class ContractProcessor extends BlockchainProcessor {
         );
       }
 
-      if (deploymentSpec.state?.interactsWith)
+      const ozClient = new OpenZeppelinClient(this.logger);
+      if (
+        entity.spec.network !== 'near' &&
+        !this.isCacheUpToDate(deploymentSpec.rbac)
+      ) {
+        await this.runExclusive(
+          'deployment-rbac-fetch',
+          deployment.address,
+          async _logger => {
+            const accessControl = await ozClient.getContractAccessControl(
+              deployment.address,
+            );
+            const accountRoles = await ozClient.getAccountRoles(
+              deployment.address,
+            );
+            if (accessControl?.roles || accountRoles?.membership) {
+              deploymentSpec.rbac = {
+                roles: accessControl?.roles,
+                membership: accountRoles?.membership,
+                fetchDate: new Date().getTime(),
+              };
+            }
+          },
+        );
+      }
+
+      if (deploymentSpec.state?.interactsWith) {
         for (const [role, val] of Object.entries(
           deploymentSpec.state.interactsWith,
         )) {
@@ -120,26 +160,14 @@ export class ContractProcessor extends BlockchainProcessor {
             this.logger.debug(err);
           }
         }
-
-      entity.spec.deployment = deploymentSpec;
-      if (deploymentSpec.source?.abi)
-        entity.spec.definition = deploymentSpec.source.abi;
+      }
     }
 
-    return entity;
-  }
-
-  async postProcessEntity?(
-    entity: Entity,
-    location: LocationSpec,
-    emit: CatalogProcessorEmit,
-    _cache: CatalogProcessorCache,
-  ): Promise<Entity> {
-    if (isContractComponent(entity)) {
-      await this.processContractComponent(entity, location, emit);
-    } else if (isContractDeployment(entity)) {
-      await this.processContractDeployment(entity, location, emit);
+    entity.spec.deployment = deploymentSpec;
+    if (entity.spec.deployment?.source?.abi) {
+      entity.spec.definition = entity.spec.deployment.source.abi;
     }
+
     return entity;
   }
 }
