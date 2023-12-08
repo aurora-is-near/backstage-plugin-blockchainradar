9ad479703a58f3e85f060fb0358d94bde2b6b88f
Diego Figueroa <diego.figueroa@aurora.dev>
2023-12-08 18:12:26 -0400

    feat: add inheritable lifecycle to contract and multisigs

--- plugins/blockchainradar-backend/src/entities/MultisigDeployment.ts
+++ plugins/blockchainradar-backend/src/entities/MultisigDeployment.ts
@@ -24,6 +24,7 @@ export class MultisigDeployment extends ContractDeployment {
   entitySpec() {
     return {
       ...super.entitySpec(),
+      lifecycle: this.parent.spec?.lifecycle ? this.parent.spec.lifecycle : this.entityLifecycle(),
       type: `multisig-deployment`,
       multisig: {},
     };
